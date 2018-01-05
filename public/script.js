8d3077b27dccc974036a8737bb45a2d1a377e3d0
Lawrence Caulker <locaulker@gmail.com>
2018-01-05 09:19:08 -0500

    building shopping cart: hide cart if empty completed

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log('It works.');
+// View Constructor
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { title: 'Item 1', },
+      { title: 'Item 2', },
+      { title: 'Item 3' }
+    ],
+    cart: []
+  },
+  methods: {
+    addItem: function(index) {
+      this.total += 9.99;
+      this.cart.push(this.items[index]);
+      console.log(this.cart.length);
+    }
+  }
+});
